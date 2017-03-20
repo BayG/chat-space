f3a199067848cdc2a8b1ddf80285aa98c125ec29
Aziz Abdullah <a.wasps.tigress@gmail.com>
2017-03-20 15:46:32 +0900

    Reorganized controller: messages
    -  Define creation of instance of class MessagesController in index
    action
    - Defined create action (still no valid contents)

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,11 @@
 class MessagesController < ApplicationController
 
   def index
+    @message = Message.new
+    # @group = Group.find(params[:group_id])
   end
 
+  def create
+    # Message.create(body: params[:body], image: params[:image], user_id: params[:user_id], group_id: params[:group_id])
+  end
 end
