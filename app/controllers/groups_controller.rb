9c93f45bc8b43a9bc3073ff0c6615f564c675f31
Aziz Abdullah <a.wasps.tigress@gmail.com>
2017-03-20 15:59:22 +0900

    Reorganized controller: groups
    -  Defined Group.all in action index.
    - Defined action new:
     = Creation of an instance of class GroupsController.
     = have set current_user
    - action edit (empty)
    - Defined action create:
     = Have set conditional branch
    - Private method: defined group_params

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,11 +1,28 @@
 class GroupsController < ApplicationController
 
-def new
-  @group = Group.new
-  @user = current_user
-end
+  def index
+    @groups = Group.all
+  end
 
-def edit
-end
+  def new
+    @group = Group.new
+    @user = current_user
+  end
 
+  def edit
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+    def group_params
+      params.require(:group).permit(:group_name)
+    end
 end
