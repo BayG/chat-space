663e45b3f7da391d7f6d38db75d4ed0931aac19e
Aziz Abdullah <a.wasps.tigress@gmail.com>
2017-03-20 16:21:15 +0900

    Reconfigure routing settings

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root      'messages#index'
-  resources :groups, only: [:new, :edit]
-  resources :messages, only: [:index]
+  resources :groups, only: [:index, :new, :edit, :create] do
+    resources :messages, only: [:index]
+  end
 end
