fd665fab7df9cc8e2f4f01630c95212fd1ad3b37
Aziz Abdullah <a.wasps.tigress@gmail.com>
2017-04-17 18:43:10 +0900

    Prepared validation of :name length for testing Message model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :group_users
   has_many :messages
 
+validates :name, presence: true, length: { maximum: 6 }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
