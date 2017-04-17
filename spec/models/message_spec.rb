b81d757e117151ebdef503c1ff44545e63c2038d
Aziz Abdullah <a.wasps.tigress@gmail.com>
2017-04-17 20:36:57 +0900

    Corrected Include Matcher's English text to Japanese text - spec/models/message_spec.rb

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 describe Message do
 	describe '#create' do
 		it 'is invalid without a body' do
-			message = build(:message, body: "")
+			message = build(:message, body: nil)
 			message.valid?
-			expect(message.errors[:body]).to include("can't be blank")
+			expect(message.errors[:body]).to include("を入力してください")
 		end
 	end
 end
