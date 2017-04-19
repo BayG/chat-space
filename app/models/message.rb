474897115dedaaee3ccdf3c7fbfbb24c332389ed
Aziz Abdullah <a.wasps.tigress@gmail.com>
2017-04-20 02:17:39 +0900

    Enabled us for testing :group_id validation

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,5 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :group
 
-  validates_presence_of :body, :user_id
+  validates_presence_of :body, :user_id, :group_id
 end
--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -2,7 +2,6 @@ FactoryGirl.define do
 
   factory :message do
     body							"Hello"
-    image							"sample.png"
     user_id						"1"
     group_id					"1"
   end
