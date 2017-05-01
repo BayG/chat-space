517a15e7d7fd7060c9815ae15a78c2cbc5151918
Aziz Abdullah <a.wasps.tigress@gmail.com>
2017-05-01 12:42:31 +0700

    [Fix] Replaced tab indent to space indent
    Due to the inconvenience occurred in GitHub code reference: Showed as 4
    Tab Size in spite of 2 Tab Size setting in my own text editor 'Sublime
    text'

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,29 +1,28 @@
 require 'rails_helper'
 
 describe Message do
-	describe '#create' do
-		it 'is valid with a body, user_id, group_id' do
-			message = build(:message)
-			expect(message).to be_valid
-		end
+  describe '#create' do
+    it 'is valid with a body, user_id, group_id' do
+      message = build(:message)
+      expect(message).to be_valid
+    end
 
-		it 'is invalid without a body' do
-			message = build(:message, body: nil)
-			message.valid?
-			expect(message.errors[:body]).to include("を入力してください")
-		end
+    it 'is invalid without a body' do
+      message = build(:message, body: nil)
+      message.valid?
+      expect(message.errors[:body]).to include("を入力してください")
+    end
 
-		it 'is invalid without a user_id' do
-			message = build(:message, user_id: nil)
-			message.valid?
-			expect(message.errors[:user_id]).to include("を入力してください")
-		end
+    it 'is invalid without a user_id' do
+      message = build(:message, user_id: nil)
+      message.valid?
+      expect(message.errors[:user_id]).to include("を入力してください")
+    end
 
-		it 'is invalid without a group_id' do
-			message = build(:message, group_id: nil)
-			message.valid?
-			expect(message.errors[:group_id]).to include("を入力してください")
-		end
-	end
-
+    it 'is invalid without a group_id' do
+      message = build(:message, group_id: nil)
+      message.valid?
+      expect(message.errors[:group_id]).to include("を入力してください")
+    end
+  end
 end
