8d96a8a5386a6b8506e3fe70516a1f59c33bbad3
Aziz Abdullah <a.wasps.tigress@gmail.com>
2017-05-18 12:50:57 +0800

    Created Messages controller test code.

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,16 +1,62 @@
 require 'rails_helper'
 
 describe MessagesController do
+  let(:group) { create(:group) }
+  let(:groups) { create_list(:group, 3, user_id: user.id) }
+  let(:user) { create(:user) }
+  let(:message) { create(:message) }
+
+  before do
+    sign_in user
+  end
+
   describe 'GET #index' do
-    it "populates an array of messages" do
-      messages = create_list(:message, 3)
-      get :index
-      expect(assigns(:messages)).to match(messages)
+    it "renders the :index template" do
+      get :index, params: { group_id: group.id }
+      expect(responces).to render_template :index
     end
 
-    it "renders the :index template" do
-      get :index
-      expect(responces).to render_template :index
+    it "assigns the required contact to @user" do
+      get :index, params: { group_id: group }
+      expect(assigns(:user)).to eq user
+    end
+
+    it "assigns the required contact to @group" do
+      get :index, params: { group_id: group }
+      expect(assigns(:group)).to eq group
+    end
+
+    it "assigns the required contact to @groups" do
+      get :index, params: { group_id: group }
+      expect(assigns(:groups)).to eq user.groups
+    end
+  end
+
+  describe 'POST #create' do
+    context "successful submission" do
+      it 'increase in the number of data' do
+        expect {
+          post :create, params: { message: attributes_for(:message), group_id: group, user_id: user }
+        }.to change(Message, :count).by(1)
+      end
+
+      it 'redirect to group_messages_path' do
+        post :create, params: { messages: attributes_for(:message), group_id: group, user_id: user }
+        expect(response).to redirect_to group_messages_path
+      end
+    end
+
+    context "failure submission" do
+      it 'no change in the number of data' do
+        expect {
+        	post :create, params: { message: attributes_for(:message, body: nil), group_id: group, user_id: user }
+        }.not_to change(Message, :count)
+      end
+
+      it 'render the :index template' do
+        post :create, params: { message: attributes_for(:message, body: nil), group_id: group, user_id: user }
+        expect(response).to render_template :index
+      end
     end
   end
 end
