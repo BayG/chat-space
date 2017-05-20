d2489622a1b71e210353d6b459340c4808808e97
Aziz Abdullah <a.wasps.tigress@gmail.com>
2017-05-20 21:59:27 +0700

    Used before block and let() for simplification

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -5,15 +5,17 @@ describe MessagesController do
   let(:groups) { create_list(:group, 3, user_id: user.id) }
   let(:user) { create(:user) }
   let(:message) { create(:message) }
+  let(:success) { post :create, params: { message: attributes_for(:message), group_id: group, user_id: user } }
+  let(:body_nil) { post :create, params: { message: attributes_for(:message, body: nil), group_id: group, user_id: user } }
 
   before do
     sign_in user
-    get :index, params: { group_id: group.id }
+    get :index, params: { group_id: group }
   end
 
   describe 'GET #index' do
     it "renders the :index template" do
-      expect(responces).to render_template :index
+      expect(response).to render_template :index
     end
 
     it "assigns the required contact to @user" do
@@ -33,12 +35,12 @@ describe MessagesController do
     context "successful submission" do
       it 'increase in the number of data' do
         expect {
-          post :create, params: { message: attributes_for(:message), group_id: group, user_id: user }
+          success
         }.to change(Message, :count).by(1)
       end
 
       it 'redirect to group_messages_path' do
-        post :create, params: { messages: attributes_for(:message), group_id: group, user_id: user }
+        success
         expect(response).to redirect_to group_messages_path
       end
     end
@@ -46,12 +48,12 @@ describe MessagesController do
     context "failure submission" do
       it 'no change in the number of data' do
         expect {
-        	post :create, params: { message: attributes_for(:message, body: nil), group_id: group, user_id: user }
+          body_nil
         }.not_to change(Message, :count)
       end
 
       it 'render the :index template' do
-        post :create, params: { message: attributes_for(:message, body: nil), group_id: group, user_id: user }
+        body_nil
         expect(response).to render_template :index
       end
     end
